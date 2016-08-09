cc65ec35433ba5551458bbab3b82a1ff68db3ab5
Gabriel Lebec <glebec@gmail.com>
2016-08-09 11:46:30 -0400

    spelling

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ module.exports = app; // this line is only used to make testing easier.
 
 // remember to plug in your router and any other middleware you may need here.
 
-if (!module.parent) app.listen(3000); // conditional prevents a very esotetiric EADDRINUSE issue with mocha watch + supertest + npm test.
+if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
